27b1330fec4691e4b4d30b733c6b31b0854ea580
Gosha Komkov <homosaipios@mail.ru>
2023-11-14 19:06:52 +0300

    правлю модуль Rename

--- frontend/src/components/Modals/Rename.jsx
+++ frontend/src/components/Modals/Rename.jsx
@@ -16,10 +16,10 @@ const Rename = () => {
   const socket = useSocket();
   const rollbar = useRollbar();
   const inputRef = useRef(null);
-  const channalId = useSelector((state) => state.modal.extra.channalId);
+  const channelId = useSelector((state) => state.modal.extra.channelId);
   const channels = useSelector((state) => state.channelsInfo.channels);
   const existingChannels = channels.map((channel) => channel.name);
-  const oldNameChannel = channels.find((channel) => channel.id === channalId)?.name || '';
+  const oldNameChannel = channels.find((channel) => channel.id === channelId)?.name || '';
 
   const formik = useFormik({
     initialValues: { name: oldNameChannel },
@@ -34,7 +34,7 @@ const Rename = () => {
     onSubmit: async ({ name }) => {
       const filteredRename = filterWords(name);
       try {
-        await socket.renameChannel(channalId, filteredRename);
+        await socket.renameChannel(channelId, filteredRename);
         toast.success(t('notifications.renameChannel'));
         dispatch(close());
       } catch (error) {
