188f8ef829cc71059b194029052d68431e53bf4b
Gosha Komkov <homosaipios@mail.ru>
2023-11-19 15:55:41 +0300

    Добавил выделение имени авторированного юзера

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -21,10 +21,15 @@ const Messages = () => {
 
   const listMessages = currentMesseges.map((message) => (
     <div
-      className={`text-break mb-2 ${message.username === auth.user.username ? 'd-inline-block bg-light ' : ''}`}
+      className="text-break mb-2"
       key={message.id}
     >
-      <b>{message.username}</b>
+      <b
+        className={`${message.username === auth.user.username ? 'bg-light ' : ''}`}
+      >
+        {message.username}
+
+      </b>
       {`: ${message.body}`}
     </div>
   ));
